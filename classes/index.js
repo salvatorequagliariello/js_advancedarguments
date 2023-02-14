8483847d4f636fc7b6924415569a0b8f1bc721bb
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-14 20:24:44 +0100

    feat(classes): completed all the study session on classes, added notes for argumets like `getters and setters`, `inheritance`.

--- classes/index.js
+++ classes/index.js
@@ -1,5 +1,7 @@
 
 // CLASSES.
+// We can define classes ad a template for creating objects. A class is manily composed by a CONSTRUCTOR and METHODS.
+// A CONSTRUCTOR is a special method to initialize the object. Every istance of the class will have the constructor properties and methods. Everything declared outside the constructor will be part of the class prototype, and if it's not private can be accessible from every istance of the class thanks to prototypal inheritance.
 
 class User {
 
@@ -21,10 +23,15 @@ class User {
 const mike  = new User(`Mike`);
 
 
+
+
+
 // HOISTING.
-// Unlike FUNCTIONS, using the declaration method (e.g: class Circle(){}), instead of the expression method (e.g const Circle = Class(){}), will not let let the class be hoisted. There is NO HOISTING within classes. Every class should be declared (or expressed) before its use.
+// Unlike FUNCTIONS, using the declaration method (e.g: class Circle(){}), instead of the expression method (e.g const Circle = Class(){}), will not let the class be hoisted. There is NO HOISTING within classes. Every class should be declared (or expressed) before its use.
 
 // IT'S GOOD PRACTICE TO USE THE CLASS DECLARATION METHOD, not the expression one.
+
+
 
 
 
@@ -58,6 +65,8 @@ console.log(Color.colorName);
 
 
 
+
+
 // PRIVATE MEMBERS.
 // It's possible to define PRIVATE METHODS AND PROPERTIES (FIELDS) using and HASHTAG befor the declaration #.
 
@@ -81,10 +90,60 @@ class Car {
 
 
 // GETTERS AND SETTERS.
+// Getters and setters can be used in classes just like in objects, to set and get private members. Their sintax is pretty simply, in fact we use `get` for getters and `set` for setters.
+
+const _side = new WeakMap();
+
+class Square {
+
+    constructor(side) {
+        _side.set(this, side);
+    }
+
+    get side() {
+        return _side.get(this);
+    }
+
+    set side(value) {
+        _side.set(this, value);
+    }
+
+};
+
+const s = new Square(4);
 
 
 
 // INHERITANCE.
+// To create inheritance between two classes is used the word EXTENDS.
+
+// Here we define a parent class called ANIMAL.
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} make a noise!`)
+    }
+}
+
+// If there is a constructor present in the subclass (or childclass) we first need to call super() and pass in the name parameter,  before using this.
+
+class Dog extends Animal {    // Here we use the keyword extends to create inheritance with Animal. 
+    constructor(name, collar){
+        super(name);
+        this.collar = collar;          
+    }
+
+    eat() {
+        console.log(`${this.name} barks.`);
+    }
+}
+
+const fuffy = new Dog(`Fuffy`, `blue`);
+
+
 
 
 
